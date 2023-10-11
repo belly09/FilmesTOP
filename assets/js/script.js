e412d07015c4dd8890f63c57ac3b1b21172016b1
ISABEL <cg3026337@LAB202M03.IFSP>
2023-10-11 10:51:47 -0300

    Finalizando menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 //SCRIPT
+// VARIAVEIS DO NAVBAR
+
+const navOpenBtn = document.querySelector('[data-menu-open]');
+const navCloseBtn = document.querySelector('[data-menu-close]');
+const navBar = document.querySelector('[data-navbar]');
+const overlay = document.querySelector('[data-overlay]');
+
+const nav = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener('click', function() {
+    navBar.classList.toggle('active'); //abrir e fechar
+  });
+}
