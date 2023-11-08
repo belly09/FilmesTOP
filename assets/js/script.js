d955dac4b67d04b8926e8e59a75f79c513625508
ISABEL <cg3026337@LAB202M03.IFSP>
2023-11-08 11:02:51 -0300

    banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-//SCRIPT
-// VARIAVEIS DO NAVBAR
 
-const navOpenBtn = document.querySelector('[data-menu-open]');
-const navCloseBtn = document.querySelector('[data-menu-close]');
-const navBar = document.querySelector('[data-navbar]');
-const overlay = document.querySelector('[data-overlay]');
+
+var navOpenBtn = document.querySelector("[data-menu-open]");
+const navCloseBtn = document.querySelector("[data-menu-close]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
 
 const nav = [navOpenBtn, navCloseBtn, overlay];
 
-for (let i = 0; i < nav.length; i++) {
-  nav[i].addEventListener('click', function() {
-    navBar.classList.toggle('active'); //abrir e fechar
-  });
+for (let i=0; i < nav.length; i++){
+nav[i].addEventListener('click', function() {
+navbar.classList.toggle('active');
+})
 }
